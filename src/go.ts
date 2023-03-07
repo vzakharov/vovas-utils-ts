b0cf299c061d962ded2927e491d24217b6c7d4a2
Vova Zakharov <vzakharov@gmail.com>
2023-03-07 17:05:48 +0100

    go

--- src/go.ts
+++ src/go.ts
@@ -1,10 +1,10 @@
 export type GoRecurse<TReturn, TArg> = (arg: TArg) => TReturn;
 
-export type GoCallback<TReturn, TArg> = (recurse: GoRecurse<TReturn, TArg>, arg: TArg) => TReturn;
+export type GoCallback<TReturn, TArg> = (arg: TArg, recurse?: GoRecurse<TReturn, TArg>) => TReturn;
 
 export function go<TReturn, TArg>(callback: GoCallback<TReturn, TArg>, arg: TArg): TReturn {
   const recurse: GoRecurse<TReturn, TArg> = (arg: TArg) => go(callback, arg);
-  return callback(recurse, arg);
+  return callback(arg, recurse);
 }
 
 export function goer<TReturn, TArg>(callback: GoCallback<TReturn, TArg>): GoRecurse<TReturn, TArg> {
@@ -15,11 +15,11 @@ export function goer<TReturn, TArg>(callback: GoCallback<TReturn, TArg>): GoRecu
 function examples() {
 
   // 1. Function to calculate the factorial with `goer`:
-  const getFactorial = goer<number, number>((recurse, n) => n === 0 ? 1 : n * recurse(n - 1));
+  const getFactorial = goer<number, number>((n, recurse) => n === 0 ? 1 : n * recurse(n - 1));
   console.log(getFactorial(5));
   
   // 2. Retry connecting to an endpoint five times with `go`:
-  go<Promise<Response>, number>((retry, retriesLeft) => {
+  go<Promise<Response>, number>((retriesLeft, retry) => {
     try {
       return fetch("https://example.com");
     } catch (e) {
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './createEnv.js';
 export * from './doWith.js';
 export * from './download.js';
 export * from './ensure.js';
+export * from './go.js';
 export * from './humanize.js';
 export * from './jsonClone.js';
 export * from './jsonEqual.js';
