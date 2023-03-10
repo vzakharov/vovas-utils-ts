8c8a4f42f6c23fb3697c1f07e392aa97744bb632
Vova Zakharov <vzakharov@gmail.com>
2023-03-10 13:23:39 +0100

    shorter, shorterer

--- src/throwError.ts
+++ src/throwError.ts
@@ -1,9 +1,5 @@
 export function throwError<T extends Error>(error: T): never
 export function throwError(message: string): never
 export function throwError<T extends Error>(error: T | string): never {
-  if (typeof error === 'string') {
-    throw new Error(error);
-  } else {
-    throw error;
-  }
+  throw typeof error === 'string' ? new Error(error) : error;
 }
