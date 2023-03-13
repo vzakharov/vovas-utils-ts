bd10bd22203fe4a208ef5fcbeb30f99bb907592c
Vova Zakharov <vzakharov@gmail.com>
2023-03-13 15:06:11 +0100

    build

--- dist/createEnv.js
+++ dist/createEnv.js
@@ -19,7 +19,8 @@ export function createEnv(descriptor, options = {}) {
             Object.defineProperty(env, keyOfT, {
                 get() {
                     throw options.missingKeyError?.(ENV_KEY) ?? new Error(`Missing env ${ENV_KEY} (${description})`);
-                }
+                },
+                configurable: true,
             });
             console.log(`WARNING: Missing env ${ENV_KEY} (${description}). Not throwing error until it is attempted to be used.`);
         }
